3302cefb8953685511a929fb04d578d87d420a3f
LeisurelyAzure <39610187+AmberCrafter@users.noreply.github.com>
2022-08-26 10:04:48 +0000

    Fix pg datetime syntex

--- scripts/sync_forcast.py
+++ scripts/sync_forcast.py
@@ -38,6 +38,7 @@ def sync_forcast():
     last_data = db.get_last()
     if len(last_data)>0:
         # st = parse(last_data[0][1])  # string
+        st = last_data[0][1]
         st+=datetime.timedelta(seconds=1)
         st = st.strftime("%Y-%m-%dT%H:%M:%S")
     else:
